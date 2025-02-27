aa6a644c0f67610e41c0158aa824e185fed108aa
Lime <LimeProgramming64@gmail.com>
2025-02-27 22:36:42 +0000

    initial upload

---
+++ AddAndModifyComponent/OnWorldInit.cs
@@ -0,0 +1,17 @@
+﻿using FrooxEngine;
+using ResoniteModLoader;
+
+namespace AddAndModifyComponent;
+
+public partial class AddAndModifyComponent : ResoniteMod
+{
+    private static void AddComponent(World world)
+    {
+        world.RunSynchronously(() =>
+        {
+            Slot DemoSlot = world.AddSlot(world.RootSlot, "DemoSlotMod", false);
+            var Comment = DemoSlot.AttachComponent<Comment>();
+            Comment.Text.Value = "That was easy!";
+        });
+    }
+}
---
+++ AddAndModifyComponent/StartingPoint.cs
@@ -0,0 +1,22 @@
+﻿using ResoniteModLoader;
+using FrooxEngine;
+
+
+namespace AddAndModifyComponent
+{
+    public partial class AddAndModifyComponent : ResoniteMod
+    {
+        public override string Name => "AddAndModifyComponent";
+        public override string Author => "CalamityLime";
+        public override string Version => "1.0.0";
+        public override string Link => "https://github.com/LimeProgramming/AddAndModifyComponent";
+
+        public override void OnEngineInit()
+        {
+            Engine.Current.RunPostInit(() =>
+            {
+                Engine.Current.WorldManager.WorldAdded += AddComponent;
+            });
+        }
+    }
+}
